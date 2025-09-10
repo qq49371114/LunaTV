a78ad74d5b0ab55b2b6800ba16022605e4712b1f
qq49371114 <143635592+qq49371114@users.noreply.github.com>
2025-09-10 21:21:25 +0800

    Update Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,7 +2,7 @@
 
 'use client';
 
-import { Cat, Clover, Film, Home, Menu, Radio, Search, Star, Tv } from 'lucide-react';
+import { Cat, Clover, Film, Home, Menu, Radio, Search, Star, Settings,Tv } from 'lucide-react';
 import Link from 'next/link';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import {
@@ -149,6 +149,11 @@ const Sidebar = ({ onToggle, activePath = '/' }: SidebarProps) => {
       icon: Radio,
       label: '直播',
       href: '/live',
+    },
+    {
+      icon: Settings,
+      label: 'TVBox配置',
+      href: '/config',
     },
   ]);
 
